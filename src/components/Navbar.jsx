89a446f89fe5f0a1454b1abbe0d424f81cdb7f3e
Exo <paink37@hotmail.com>
2023-02-02 21:08:56 +0000

    Estrutura de navbar concluida

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>
+        <a href="/">
+          <img
+            src="https://cache.dominos.com/olo/6.102.5/assets/build/images/img/dpz-logo.svg"
+            alt="Domino's Pizza"
+          />
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a href="/pt/pages/order/">Encomenda online</a>
+        </li>
+        <li>
+          <a href="/">Menu</a>
+        </li>
+        <li>
+          <a href="/"> promoções</a>
+        </li>
+        <li>
+          <a href="/">Lojas</a>
+        </li>
+        <li>
+          <a href="/">Segue</a>
+        </li>
+        <li>
+          <a href="/">English</a>
+        </li>
+        <li>
+          <a href="/">franchising</a>
+        </li>
+        <li>
+          <a href="/">login</a>
+        </li>
+        <li>
+          <a href="/">carrinho</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
